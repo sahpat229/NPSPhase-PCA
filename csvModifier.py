fd19af65ac5682c63c879f852934fbfc01f5ecbc
sahpat229 <sahpat229@gmail.com>
2016-07-18 21:43:04 -0400

    Fixed csvModifier.py

--- csvModifier.py
+++ csvModifier.py
@@ -1,6 +1,8 @@
 import csv
 import datetime
 import re
+import random
+
 """
 extracts from the CSV only the "important" columns and
 puts it in the outfile csv
@@ -24,6 +26,70 @@ keystrings = [
 	"numberofemployees",
 	"annualrevenue",
 ]
+
+
+def breachRoll():
+	x = random.uniform(0, 3)
+	if 0 <= x <= 1:
+		return 0
+	elif 1 < x <= 2:
+		return 1
+	else:
+		return 2
+
+mapper = {
+	
+	'MUOS': {
+
+		2008 : {
+
+			1 : 1,
+			2 : 1,
+			3 : 0,
+			4 : 0,
+			5 : 0,
+			6 : 0,
+			7 : 0,
+			8 : 0,
+			9 : 0,
+			10 : 0,
+			11 : 0,
+			12 : 0,
+		},
+
+		2009 : {
+
+			1 : 1,
+			2 : 0,
+			3 : 0,
+			4 : 0,
+			5 : 0,
+			6 : 0,
+			7 : 0,
+			8 : 0,
+			9 : 0,
+			10 : 0,
+			11 : 0,
+			12 : 0
+		},
+
+		2010 : {},
+		2011 : {},
+		2012 : {},
+		2013 : {},
+		2014 : {},
+		2015 : {},
+		2016 : {},
+	}
+}
+
+years = [2010, 2011, 2012, 2013, 2014, 2015, 2016]
+months = range(1, 13)
+
+for year in years:
+	for month in months:
+		mapper['MUOS'][year][month] = breachRoll()
+
 
 """
 Properly changes discrete string fields to numeric fields
@@ -84,21 +150,75 @@ takes the input USA Spending data, modifies it, and puts it
 in the output csvfile named by outfile
 """
 
+class YearException(Exception):
+	def __init__(self, value):
+		self.value = value
+
+def get_range_of_dates(start_month, start_year, end_month, end_year):
+
+
+	if end_year > start_year:
+		range_of_months = (zip(range(start_month, 13), [start_year] * \
+			len(range(start_month, 13))))
+		curr_year = start_year + 1
+		while (curr_year < end_year):
+			range_of_months + (zip(range(1, 13), [curr_year] * 12))
+			curr_year += 1
+		range_of_months + (zip(range(1, end_month + 1), [end_year] * 12))
+		return range_of_months
+
+	elif start_year == end_year :
+		if end_month < start_month:
+			raise YearException('N/A')
+		else:
+			range_of_months = zip(range(start_month, end_month + 1), [start_year] * \
+				len(range(start_month, end_month + 1)))
+			return range_of_months
+
+	elif end_year < start_year:
+		raise YearException('N/A')
+
 def modify_CSV(infile, outfile, company_name):
 	with open(infile, 'r') as csvinfile, open(outfile, 'w+') as csvoutfile:
 		csvinfile_reader = csv.DictReader(csvinfile)
-		csvoutfile_writer = csv.DictWriter(csvoutfile, fieldnames=keystrings, lineterminator='\n')
+		added_names = keystrings + ["Breach"]
+		csvoutfile_writer = csv.DictWriter(csvoutfile, fieldnames=added_names, lineterminator='\n')
 		csvoutfile_writer.writeheader()
 		for row in csvinfile_reader:
 
 			if re.search(company_name, row["VendorName"]) == None:
 				continue
 
-			for fieldname in keystrings:
-				modify(fieldname, row)
+			[start_month, start_day, start_year] = map(int, re.split('/', row['effectivedate']))
+			print "start: ", [start_month, start_day, start_year]
+			[end_month, end_day, end_year] = map(int, re.split('/', row['currentcompletiondate']))
+			print "end: ", [end_month, end_day, end_year]
+			avg_breach = 0.0;
+			try:
+				range_of_months = get_range_of_dates(start_month, start_year, end_month, end_year)
+				print "RANGE OF MONTHS: ", range_of_months
+				total_breach = 0.0
+				for tup in range_of_months:
+					print "tup", tup
+					total_breach += mapper['MUOS'][tup[1]][tup[0]]
+				avg_breach = total_breach/(len(range_of_months))
 
-			inrow_data = {key: row[key] for key in keystrings}
-			csvoutfile_writer.writerow(inrow_data)
+			except YearException as err:
+
+				for fieldname in keystrings:
+					modify(fieldname, row)
+
+				inrow_data = {key: row[key] for key in keystrings}
+				inrow_data['Breach'] = err.value
+				csvoutfile_writer.writerow(inrow_data)
+
+			else:
+				for fieldname in keystrings:
+					modify(fieldname, row)
+
+				inrow_data = {key: row[key] for key in keystrings}
+				inrow_data['Breach'] = avg_breach
+				csvoutfile_writer.writerow(inrow_data)
 			# inrow_data = {
 			# 	"active" : row["active"],
 			# 	"dollarsobligated" : row["dollarsobligated"],
